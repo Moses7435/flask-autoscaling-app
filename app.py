56c926ed078b43696c9fa0812ec0b15f5188c767
moses7435 <meclib12@gmail.com>
2026-02-11 18:45:12 +0530

    My YouTube Inside Flask Page

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Flask App Running Successfully with Docker + Auto Scaling!"
-
-@app.route("/health")
-def health():
-    return "OK", 200
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
